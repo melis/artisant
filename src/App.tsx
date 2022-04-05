56b2e47db91924a539b611f11a2ae25bcd26a31f
Melis <59087686+melis@users.noreply.github.com>
2022-04-05 20:15:24 +0600

    5

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import './App.css';
-import Product from './components/Product/Product';
-import { getProducts } from './store/products/productsAction';
-
+import "./App.css";
+import Product from "./components/Product/Product";
+import { getProducts } from "./store/products/productsAction";
 
 function App() {
-  const {products, load}=useSelector(a=>a)
-  const dispatch=useDispatch()
-  useEffect(()=>{
-    dispatch(getProducts())
-  }, [dispatch])
+  const { products, load } = useSelector((a) => a);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
 
-  if (load) {return <div>Loading...</div>}
+  if (load) {
+    return <div>Loading...</div>;
+  }
 
- 
   return (
     <div className="container">
-      <h1 className='title'>Explore</h1>
-      <p className='text'>Buy and sell digital fashion NFT art</p>
-    <div className='cards'>
-    {products.map(p=><Product product={p}  key={p.product_id}/>)}
-    </div>
-     
+      <h1 className="title">Explore</h1>
+      <p className="text">Buy and sell digital fashion NFT art</p>
+      <div className="cards">
+        {products.map((p) => (
+          <Product product={p} key={p.product_id} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,24 +1,28 @@
-import React, { FC } from 'react';
-import { ProductType } from '../../store/products/productsReduser';
+import React, { FC } from "react";
+import { ProductType } from "../../store/products/productsReduser";
 
-import style from './Product.module.scss';
+import style from "./Product.module.scss";
 
-type PropsType={
-    product: ProductType
-}
+type PropsType = {
+  product: ProductType;
+};
 
-const  Product: FC<PropsType> =({product})=> {
-    
-    return (
-        <div className={style.card}>
-            <div className=''>
-                 <img src="/images/card.jpg" alt="" />
-            </div>
-            <div className='card_info'>
-                {product.latest_price}
-            </div>
+const Product: FC<PropsType> = ({ product }) => {
+  console.log(product);
+  return (
+    <div className={style.card}>
+      <div className={style.imgBox}>
+        <div className={style.created_by}>
+          <div>created_by</div>
+          <div>{product.created_by.display_name}</div>
         </div>
-    );
-}
+
+        <img src="/images/card.jpg" alt="" />
+      </div>
+
+      <div className="card_info">{product.latest_price}</div>
+    </div>
+  );
+};
 
 export default Product;
